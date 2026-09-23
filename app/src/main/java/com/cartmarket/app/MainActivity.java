62f588b4f184fe5af77cf1649765c1252d6738e7
1234ppiiqqww-code <1234ppiiqqww@gmail.com>
2026-09-23 15:57:39 +0900

    Update MainActivity.java

--- app/src/main/java/com/cartmarket/app/MainActivity.java
+++ app/src/main/java/com/cartmarket/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cartmarket.app;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -16,16 +17,20 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+
 public class MainActivity extends Activity {
     private static final String APP_URL = "https://cartmarket-shared.gso8022.chatgpt.site/app.html";
     private static final int FILE_CHOOSER_REQUEST = 1001;
 
+
     private WebView webView;
     private ValueCallback<Uri[]> fileCallback;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         getWindow().setStatusBarColor(Color.WHITE);
         getWindow().setNavigationBarColor(Color.WHITE);
@@ -34,89 +39,3 @@ public class MainActivity extends Activity {
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             getWindow().setDecorFitsSystemWindows(true);
-        }
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
-
-        FrameLayout root = new FrameLayout(this);
-        root.setBackgroundColor(Color.WHITE);
-        webView = new WebView(this);
-        webView.setBackgroundColor(Color.WHITE);
-        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        root.addView(webView, new FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT));
-        root.setOnApplyWindowInsetsListener((view, insets) -> {
-            int bottomInset = insets.getSystemWindowInsetBottom();
-            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) webView.getLayoutParams();
-            params.bottomMargin = bottomInset + dp(10);
-            webView.setLayoutParams(params);
-            return insets;
-        });
-        setContentView(root);
-        root.requestApplyInsets();
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(false);
-        settings.setAllowContentAccess(true);
-        settings.setMediaPlaybackRequiresUserGesture(false);
-        settings.setLoadWithOverviewMode(false);
-        settings.setUseWideViewPort(false);
-        settings.setTextZoom(88);
-        webView.setInitialScale(92);
-
-        CookieManager cookies = CookieManager.getInstance();
-        cookies.setAcceptCookie(true);
-        cookies.setAcceptThirdPartyCookies(webView, true);
-
-        webView.clearCache(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onShowFileChooser(WebView view, ValueCallback<Uri[]> callback,
-                                             FileChooserParams params) {
-                if (fileCallback != null) fileCallback.onReceiveValue(null);
-                fileCallback = callback;
-                try {
-                    startActivityForResult(params.createIntent(), FILE_CHOOSER_REQUEST);
-                } catch (Exception e) {
-                    fileCallback = null;
-                    return false;
-                }
-                return true;
-            }
-        });
-
-        if (savedInstanceState == null) webView.loadUrl(APP_URL);
-        else webView.restoreState(savedInstanceState);
-    }
-
-    private int dp(int value) {
-        return Math.round(value * getResources().getDisplayMetrics().density);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == FILE_CHOOSER_REQUEST && fileCallback != null) {
-            fileCallback.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
-            fileCallback = null;
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        webView.saveState(outState);
-        super.onSaveInstanceState(outState);
-    }
-}
